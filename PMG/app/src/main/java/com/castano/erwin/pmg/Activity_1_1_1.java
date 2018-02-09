1895e7c78c12ac7614e909de2034cdf5e41e2eb9
Erwin Felipe Castano Mora <ecastano@bancolombia.com.co>
2018-02-09 14:46:51 -0500

    Se agrega actividad 1.1.1.1

--- PMG/app/src/main/java/com/castano/erwin/pmg/Activity_1_1_1.java
+++ PMG/app/src/main/java/com/castano/erwin/pmg/Activity_1_1_1.java
@@ -3,6 +3,7 @@ package com.castano.erwin.pmg;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity_1_1_1 extends AppCompatActivity {
 
@@ -15,6 +16,12 @@ public class Activity_1_1_1 extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+        overridePendingTransition(R.anim.right_in, R.anim.right_out);
+    }
+
+    public void ingresarMovimiento(View vista){
+        Intent intencion = new Intent(this, Activity_1_1_1_1.class);
+        startActivity(intencion);
         overridePendingTransition(R.anim.right_in, R.anim.right_out);
     }
 
